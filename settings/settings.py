aeba17a4ae32030c868755d107fc2d7890f18d84
Antonio Sousa <antonioufpi@gmail.com>
2024-08-02 21:38:59 -0300

    base

--- settings/settings.py
+++ settings/settings.py
@@ -125,6 +125,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
